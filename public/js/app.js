b94467a58e372995b641edd9d84750ae21d632a4
Ibrahimboussta <ibrahimboussta@gmail.com>
2023-12-26 22:57:09 +0100

    age check

--- public/js/app.js
+++ public/js/app.js
@@ -2,63 +2,74 @@
 let profilUser = prompt('sign-up Or login Or change password')
 
 if (profilUser == 'sign-up') {
-    // let askNa = prompt(`enter your name`);
-    // while (true) {
-    //     if (askNa == "") {
-    //         askNa = prompt(`enter your name`);
-    //     } else {
-    //         break;
-    //     }
-    // }
-    // let removeName = askNa.trim();
-    // let removeSpaces = askNa.replace(/\s+/g, "");
-    // let lenthName = removeSpaces.split("");
-    // let pushName = [];
-    // let specialCaracter = `\`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~`;
-    // let spechCaracter = specialCaracter.split("")
-    // while (true) {
-    //     if (removeSpaces.length > 5) {
-    //         for (let index = 0; index < spechCaracter.length; index++) {
-    //             let element = spechCaracter[index];
-    //             if (removeSpaces.includes(element)) {
-    //                 break
-    //             } else {
-    //                 continue
-    //             }
-    //         }
-    //         let splitName = removeName.split(" ");
-    //         console.log(`yes`);
-    //         for (let index = 0; index < splitName.length; index++) {
-    //             let element = splitName[index];
-    //             let lowerName = element.toLowerCase()
-    //             let second_name = lowerName.charAt(0).toUpperCase() + lowerName.slice(1);
-    //             pushName.push(second_name)
-    //         }
-    //         break
-    //     } else {
-    //         askNa = prompt(`enter your name`);
-    //         removeSpaces = askNa.replace(/\s+/g, "");
-    //         removeName = askNa.trim();
-    //     }
-    // }
-    // console.table(pushName);
-    // let sec_Name = pushName.join(" ")
-    // console.table(sec_Name);
+    let askNa = prompt(`enter your name`);
+    while (true) {
+        if (askNa == "") {
+            askNa = prompt(`enter your name`);
+        } else {
+            break;
+        }
+    }
+    let removeName = askNa.trim();
+    let removeSpaces = askNa.replace(/\s+/g, "");
+    let lenthName = removeSpaces.split("");
+    let pushName = [];
+    let specialCaracter = `\`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~`;
+    let spechCaracter = specialCaracter.split("")
+    while (true) {
+        if (removeSpaces.length > 5) {
+            for (let index = 0; index < spechCaracter.length; index++) {
+                let element = spechCaracter[index];
+                if (removeSpaces.includes(element)) {
+                    break
+                } else {
+                    continue
+                }
+            }
+            let splitName = removeName.split(" ");
+            console.log(`yes`);
+            for (let index = 0; index < splitName.length; index++) {
+                let element = splitName[index];
+                let lowerName = element.toLowerCase()
+                let second_name = lowerName.charAt(0).toUpperCase() + lowerName.slice(1);
+                pushName.push(second_name)
+            }
+            break
+        } else {
+            askNa = prompt(`enter your name`);
+            removeSpaces = askNa.replace(/\s+/g, "");
+            removeName = askNa.trim();
+        }
+    }
+    console.table(pushName);
+    let sec_Name = pushName.join(" ")
+    console.table(sec_Name);
 
     // let putEmail = prompt('entre your email')
     // console.log(putEmail.trim().toLowerCase());
 
     let emailUser = prompt('entre your email')
-    // if (emailUser.includes(' ')) {
-    //     prompt('please entre whitout spaces')
-    // }
 
-    while (emailUser.includes(" ") || emailUser.length < 10) {
+    while (emailUser.includes(" ") || emailUser.length < 10 || emailUser.includes("@")) {
         emailUser = prompt('entre your email')
         // break;
-    }
+    }  
     
     console.log(emailUser.trim().toLowerCase());
+
+    //* age checked : 
+
+    let ageUser = prompt('please entre your age')
+    let alphaExp = /^[0-9a-zA-Z]+$/;
+    while ((ageUser.includes(" ")) || (ageUser == "" ) || (ageUser == 0 ) || (isNaN(ageUser))) {
+        ageUser = prompt('try again')
+        // if (isNaN(ageUser)) {
+        //     ageUser = prompt('try again')
+        // }
+    }
+    console.log(ageUser.trim());
+
+
 }
 
 
